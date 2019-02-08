89436723ee3e0dd0b876a15dd9e4b24dbf6f1a15
chrisrosana <chrisrosana@yahoo.com>
2019-02-07 20:02:06 -0800

    init commit

---
+++ Flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  Flixster
+//
+//  Created by Christopher Rosana on 2/7/19.
+//  Copyright © 2019 chrisrosana. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
