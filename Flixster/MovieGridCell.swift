44001c82da7fc5d335020302a9c865680e920f53
chrisrosana <chrisrosana@yahoo.com>
2019-02-13 00:16:22 -0800

    added movie details screen and collection view

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Christopher Rosana on 2/12/19.
+//  Copyright © 2019 chrisrosana. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
